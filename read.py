5aa36a77087785d7f27c1b0e39b8f2f3461f6006
unknown <pedro_gonsalves@hotmail.com>
2024-02-24 18:35:22 -0300

    fix: file name in csv results

--- read.py
+++ read.py
@@ -1,19 +1,19 @@
-from main import LocalSparkSession, fileFormats, list_files, save_result
+from main import LocalSparkSession, fileFormats, list_files, save_result, get_root_file
 import time
 
 
 def main() -> None:
     ss = LocalSparkSession().spark_session()
+    _, fileName = get_root_file()
 
     for fileFormat in fileFormats:
         allFiles = list_files(fileFormat)
         for file in allFiles:
-            fileName = file.split("\\")[1].split(".")[0]
             start = time.time()
             dfMain = ss.read.option("header", "true").format(fileFormat).load(file)
             dfMain.show(5)
             end = time.time()
-            save_result('read', fileFormat, float(f"{end - start:.2f}"), fileName)
+            save_result('read', fileFormat, 'sec', float(f"{end - start:.2f}"), fileName)
 
 
 if __name__ == "__main__":
