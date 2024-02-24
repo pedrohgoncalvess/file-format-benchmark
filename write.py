4b6260d53be10ea94a3eb45bad0f501ca5ab8f64
unknown <pedro_gonsalves@hotmail.com>
2024-02-24 18:36:36 -0300

    fix: remove root file iteration

--- write.py
+++ write.py
@@ -1,20 +1,18 @@
-from main import LocalSparkSession, save_result, fileFormats, list_root_files
+from main import LocalSparkSession, save_result, fileFormats, get_root_file
 import time
 
 
 def main() -> None:
     ss = LocalSparkSession().spark_session()
-    rootFiles = list_root_files()
+    rootFile, fileName = get_root_file()
 
-    for rootFile in rootFiles:
-        dfMain = ss.read.option("header", "true").csv(rootFile)
-        fileName = rootFile.split("\\")[1].split(".")[0]
-        for fileFormat in fileFormats:
-            start = time.time()
-            dfMain.repartition(1).write.mode("overwrite").option("header", "true").format(fileFormat).save(
-                f'datasets/{fileFormat}')
-            end = time.time()
-            save_result('write', fileFormat, float(f"{end - start:.2f}"), fileName)
+    dfMain = ss.read.option("header", "true").csv(rootFile)
+    for fileFormat in fileFormats:
+        start = time.time()
+        dfMain.repartition(1).write.mode("overwrite").option("header", "true").format(fileFormat).save(
+            f'datasets/{fileFormat}')
+        end = time.time()
+        save_result('write', fileFormat, 'sec', float(f"{end - start:.2f}"), fileName)
 
 
 if __name__ == "__main__":
