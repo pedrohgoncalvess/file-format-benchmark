55c369c0b539b2d8212d2c4d0688f5161debe360
unknown <pedro_gonsalves@hotmail.com>
2024-02-28 16:53:48 -0300

    refac: rename all column name to better pattern and stop spark after execution

--- write.py
+++ write.py
@@ -1,5 +1,6 @@
-from main import LocalSparkSession, save_result, fileFormats, get_root_file
+from configs import LocalSparkSession, save_result, fileFormats, get_root_file
 import time
+from pyspark.sql.functions import col, lower
 
 
 def main() -> None:
@@ -8,12 +9,12 @@ def main() -> None:
 
     dfMain = ss.read.option("header", "true").csv(rootFile)
     for fileFormat in fileFormats:
+        print(f"Writing {fileFormat} file.")
         start = time.time()
-        dfMain.repartition(1).write.mode("overwrite").option("header", "true").format(fileFormat).save(
+        newDf = dfMain.select([col(column_name).alias(column_name.replace(" ", "_").lower()) for column_name in dfMain.columns])
+        newDf.repartition(1).write.mode("overwrite").option("header", "true").format(fileFormat).save(
             f'datasets/{fileFormat}')
         end = time.time()
         save_result('write', fileFormat, 'sec', float(f"{end - start:.2f}"), fileName)
 
-
-if __name__ == "__main__":
-    main()
+    ss.stop()
