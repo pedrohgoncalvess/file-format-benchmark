274009346bc28d52fd53aa08f7c50d902c36031a
unknown <pedro_gonsalves@hotmail.com>
2024-03-09 19:41:36 -0300

    refac: separate better the functions of scripts

---
+++ operations/write.py
@@ -0,0 +1,21 @@
+from utils.file_manage import save_result, fileFormats, get_root_file
+from utils.spark_local import LocalSparkSession
+import time
+from pyspark.sql.functions import col
+
+
+def main() -> None:
+    ss = LocalSparkSession().spark_session()
+    rootFile, fileName = get_root_file()
+
+    dfMain = ss.read.option("header", "true").csv(rootFile)
+    for fileFormat in fileFormats:
+        print(f"Writing {fileFormat} file.")
+        start = time.time()
+        newDf = dfMain.select([col(column_name).alias(column_name.replace(" ", "_").lower()) for column_name in dfMain.columns])
+        newDf.repartition(1).write.mode("overwrite").option("header", "true").format(fileFormat).save(
+            f'datasets/{fileFormat}')
+        end = time.time()
+        save_result('write', fileFormat, 'sec', float(f"{end - start:.2f}"), fileName)
+
+    ss.stop()
